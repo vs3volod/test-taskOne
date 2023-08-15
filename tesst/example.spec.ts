3c843d24767861571114af18c170f388efe4dc8f
vs3volod <lispuhv@gmail.com>
2023-08-15 10:27:43 +0300

    commit 2

--- tesst/example.spec.ts
+++ tesst/example.spec.ts
@@ -6,8 +6,8 @@ test('Gmail unread messages count', async ({ page }) => {
   await page.locator('[type="email"]').type('testertesting2032@gmail.com');
   await page.locator('#identifierNext > div > button > span').click();
   await page.waitForSelector('#passwordNext > div > button > span');
-  await page.locator('[type="password"]').type('testing2032');
+  await page.locator('[autocomplete="current-password"]').type('testing2032');
   await page.locator('//*[@id="passwordNext"]/div/button/span').click();
-  await page.waitForSelector('[class="aim ain"]');
-  console.log(await page.locator('[class="aim ain"]').allTextContents());
+  await page.waitForSelector('.aim.ain');
+  console.log(await page.locator('.aim.ain').allTextContents());
 });
